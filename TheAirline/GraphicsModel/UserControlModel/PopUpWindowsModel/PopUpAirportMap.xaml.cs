a3b56b7e44aed24146fb8cd2c3da43e4a19d484f
pjank42 <pjank42@hotmail.com>
2012-01-23 16:33:34 +0000

    Forgot a couple of classes
    git-svn-id: http://svn.code.sf.net/p/theairline/code/trunk@82 a67f925f-2c50-4282-93e8-9e6206ef1626

---
+++ TheAirline/GraphicsModel/UserControlModel/PopUpWindowsModel/PopUpAirportMap.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using TheAirline.Model.AirportModel;
+using TheAirline.Model.GeneralModel;
+
+namespace TheAirline.GraphicsModel.UserControlModel.PopUpWindowsModel
+{
+    /// <summary>
+    /// Interaction logic for PopUpAirportMap.xaml
+    /// </summary>
+    public partial class PopUpAirportMap : PopUpWindow
+    {
+        private Airport Airport;
+        //shows the pop up for an airport
+        public static void ShowPopUp(Airport airport)
+        {
+            PopUpAirportMap window = new PopUpAirportMap(airport);
+            window.ShowDialog();
+        }
+        public PopUpAirportMap(Airport airport)
+        {
+            this.Airport = airport;
+
+            InitializeComponent();
+            this.Uid = "1000";
+            
+            this.Width = 600;
+
+            this.Height = 300;
+
+            this.WindowStartupLocation = System.Windows.WindowStartupLocation.CenterScreen;
+
+            Image imgMap = new Image();
+            imgMap.Width = 600;
+            imgMap.Height = 300;
+            imgMap.Source = new BitmapImage(new Uri(this.Airport.Profile.Map, UriKind.RelativeOrAbsolute));
+            RenderOptions.SetBitmapScalingMode(imgMap, BitmapScalingMode.HighQuality);
+
+            this.Content = imgMap;
+
+        }
+    }
+}
