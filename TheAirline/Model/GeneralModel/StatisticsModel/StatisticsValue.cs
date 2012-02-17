226eadde90607d37793ebe85938aba2c722efaa8
pjank42 <pjank42@hotmail.com>
2012-02-17 20:18:13 +0000

    Added missing files
    git-svn-id: http://svn.code.sf.net/p/theairline/code/trunk@162 a67f925f-2c50-4282-93e8-9e6206ef1626

---
+++ TheAirline/Model/GeneralModel/StatisticsModel/StatisticsValue.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace TheAirline.Model.GeneralModel.StatisticsModel
+{
+    public class StatisticsValue
+    {
+        public StatisticsType Stat { get; set; }
+        public int Value { get; set; }
+        public StatisticsValue(StatisticsType stat, int value)
+        {
+            this.Value = value;
+            this.Stat = stat;
+        }
+    }
+}
