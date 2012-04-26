d1f81a5d5cd88b0a47cfb3bd9f5547ed87f5cef9
pjank42 <pjank42@hotmail.com>
2012-04-26 20:41:47 +0000

    Updated showing of the timetable for a fleet airliner
    git-svn-id: http://svn.code.sf.net/p/theairline/code/trunk@262 a67f925f-2c50-4282-93e8-9e6206ef1626

--- TheAirline/GraphicsModel/UserControlModel/PopUpWindowsModel/PopUpAirlinerRoutes.xaml.cs
+++ TheAirline/GraphicsModel/UserControlModel/PopUpWindowsModel/PopUpAirlinerRoutes.xaml.cs
@@ -38,9 +38,9 @@ namespace TheAirline.GraphicsModel.UserControlModel.PopUpWindowsModel
 
             this.Title = this.Airliner.Name ;
 
-            this.Width = 800;
+            this.Width = 1200;
 
-            this.Height = 600;
+            this.Height = 250;
 
             this.WindowStartupLocation = System.Windows.WindowStartupLocation.CenterScreen;
 
@@ -49,43 +49,87 @@ namespace TheAirline.GraphicsModel.UserControlModel.PopUpWindowsModel
 
             ListBox lbRoutes = new ListBox();
             lbRoutes.ItemContainerStyleSelector = new ListBoxItemStyleSelector();
-            //lbRoutes.SetResourceReference(ListBox.ItemTemplateProperty, "QuickInfoItem");
-            lbRoutes.MaxHeight = 500;
-
+            lbRoutes.SetResourceReference(ListBox.ItemTemplateProperty, "QuickInfoItem");
+        
             mainPanel.Children.Add(lbRoutes);
 
-            foreach (Route route in this.Airliner.Routes)
+            foreach (DayOfWeek day in Enum.GetValues(typeof(DayOfWeek)))
             {
-                //lbRoutes.Items.Add(new QuickInfoValue(string.Format("{0}-{1}",route.Destination1.Profile.IATACode,route.Destination2.Profile.IATACode),createRoutePanel(route)));
-                lbRoutes.Items.Add(createRoutePanel(route));
+               lbRoutes.Items.Add(new QuickInfoValue(day.ToString(),createRoutePanel(day)));
+         
             }
 
             this.Content = mainPanel;
         }
-        //creates the panel for a route
-        private Canvas createRoutePanel(Route route)
+        //creates the panel for a route for a day
+        private Border createRoutePanel(DayOfWeek day)
         {
-             Canvas cnvFlights = new Canvas();
-             cnvFlights.Height = 20;
-      
-            foreach (RouteTimeTableEntry e in route.TimeTable.Entries.FindAll(e => e.Airliner == this.Airliner))
+            Border brdDay = new Border();
+            brdDay.BorderThickness = new Thickness(1,1,1,1);
+            brdDay.BorderBrush = Brushes.Black;
+
+            Canvas cnvFlights = new Canvas();
+            cnvFlights.Width = 24 * 60 / 2;
+            cnvFlights.Height = 20;
+            brdDay.Child = cnvFlights;
+        
+            List<RouteTimeTableEntry> entries = this.Airliner.Routes.SelectMany(r => r.TimeTable.Entries.FindAll(e => e.Airliner == this.Airliner && e.Day == day)).ToList(); //&& e.Time.Hours<12)).ToList();
+       
+            foreach (RouteTimeTableEntry e in entries)
             {
+                double maxTime = new TimeSpan(24, 0, 0).Subtract(e.Time).TotalMinutes;
+                
                 TimeSpan flightTime = MathHelpers.GetFlightTime(e.TimeTable.Route.Destination1.Profile.Coordinates, e.TimeTable.Route.Destination2.Profile.Coordinates, this.Airliner.Airliner.Type);
 
                 ContentControl ccFlight = new ContentControl();
                 ccFlight.ContentTemplate = this.Resources["FlightItem"] as DataTemplate;
-                ccFlight.Content = new KeyValuePair<double, RouteTimeTableEntry>(flightTime.TotalMinutes, e);
+                ccFlight.Content = new KeyValuePair<double, RouteTimeTableEntry>(Math.Min(flightTime.TotalMinutes/2,maxTime/2), e);
                 int minutes = 15 * (e.Time.Minutes / 15);
-                int hours = e.Time.Hours>=12 ? e.Time.Hours-12 : e.Time.Hours;
-                //Canvas.SetTop(ccFlight, 60 * e.Time.Hours + e.Time.Minutes);
-                
-                Canvas.SetLeft(ccFlight, 60*hours +e.Time.Minutes);
-
-           
+                int hours = e.Time.Hours;
+            
+                Canvas.SetLeft(ccFlight, 30*hours +e.Time.Minutes/2);
+                           
                 cnvFlights.Children.Add(ccFlight);
             }
 
-            return cnvFlights;
+            List<RouteTimeTableEntry> dayBeforeEntries = this.Airliner.Routes.SelectMany(r => r.TimeTable.Entries.FindAll(e => e.Airliner == this.Airliner && e.Day == day - 1)).ToList();
+
+            foreach (RouteTimeTableEntry e in dayBeforeEntries)
+            {
+                TimeSpan flightTime = MathHelpers.GetFlightTime(e.TimeTable.Route.Destination1.Profile.Coordinates, e.TimeTable.Route.Destination2.Profile.Coordinates, this.Airliner.Airliner.Type);
+
+                TimeSpan endTime = e.Time.Add(flightTime);
+                if (endTime.Days == 1)
+                {
+                    ContentControl ccFlight = new ContentControl();
+                    ccFlight.ContentTemplate = this.Resources["FlightItem"] as DataTemplate;
+                    ccFlight.Content = new KeyValuePair<double, RouteTimeTableEntry>(endTime.Subtract(new TimeSpan(1,0,0,0)).TotalMinutes/2, e);
+                 
+                    Canvas.SetLeft(ccFlight, 0);
+
+                    cnvFlights.Children.Add(ccFlight);
+                }
+            }
+
+            return brdDay;
         }
     }
+    //the converter for getting the end time for a route entry
+    public class RouteEntryEndTimeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+
+            RouteTimeTableEntry e = (RouteTimeTableEntry)value;
+            TimeSpan flightTime = MathHelpers.GetFlightTime(e.TimeTable.Route.Destination1.Profile.Coordinates, e.TimeTable.Route.Destination2.Profile.Coordinates, e.Airliner.Airliner.Type);
+            
+            return e.Time.Add(flightTime);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return value;
+        }
+    }
+   
 }
