6d80540607ded8e36c965455e18bc5fc94f12a68
pjank42 <pjank42@hotmail.com>
2012-09-21 06:04:29 +0000

    - forgot a few files
    git-svn-id: http://svn.code.sf.net/p/theairline/code/trunk@574 a67f925f-2c50-4282-93e8-9e6206ef1626

---
+++ TheAirline/Model/GeneralModel/CountryModel/TownModel/Town.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TheAirline.Model.GeneralModel.CountryModel.TownModel
+{
+    //the class for a town / class
+    public class Town
+    {
+        public string Name { get; set; }
+        public Country Country { get; set; }
+        public State State { get; set; }
+        public Town(string name, Country country) : this(name,country,null)
+        {
+            
+        }
+        public Town(string name, Country country, State state)
+        {
+            this.Name = name;
+            this.Country = country;
+            this.State = state;
+        }
+      
+    }
+   
+}
