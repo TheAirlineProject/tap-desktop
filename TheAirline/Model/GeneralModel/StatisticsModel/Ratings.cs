1854c64db40445130a6f8aedf9f29d18ace1d32a
mjdugan14 <mike@theairlineproject.com>
2013-01-21 19:24:04 +0000

    added ratings class with variance and standard dev methods
    git-svn-id: http://svn.code.sf.net/p/theairline/code/trunk@1040 a67f925f-2c50-4282-93e8-9e6206ef1626

---
+++ TheAirline/Model/GeneralModel/StatisticsModel/Ratings.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Math;
+
+namespace TheAirline.Model.StatisticsModel
+{
+    //the general class for ratings
+   public class Ratings
+    {
+       //standard deviation calculation
+       public static double getStandardDev(List<double> population)
+       {
+           double M = 0.0;
+           double S = 0.0;
+           int k = 1;
+           foreach (double value in population)
+           {
+               double tmpM = M;
+               M += (value - tmpM) / k;
+               S += (value - tmpM) * (value - M);
+               k++;
+           }
+           return Math.Sqrt(S / (k - 1));
+       }
+
+       //variance calculation
+       public static double getVariance(List<double> population)
+       {
+           double M = 0.0;
+           double S = 0.0;
+           int k = 1;
+           foreach (double value in population)
+           {
+               double tmpM = M;
+               M += (value - tmpM) / k;
+               S += (value - tmpM) * (value - M);
+               k++;
+           }
+           return S / (k - 1);
+       }
+    }
+}
