740227a3f7645d28603186d872236c4f47dbedcc
mjdugan14 <mike@theairlineproject.com>
2013-03-10 03:28:54 +0000

    work on finances page
    git-svn-id: http://svn.code.sf.net/p/theairline/code/trunk@1275 a67f925f-2c50-4282-93e8-9e6206ef1626

--- TheAirline/GraphicsModel/PageModel/PageFinancesModel/PageFinances.xaml.cs
+++ TheAirline/GraphicsModel/PageModel/PageFinancesModel/PageFinances.xaml.cs
@@ -1,13 +1,19 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Markup;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 using TheAirline.Model.AirlineModel;
 using TheAirline.Model.GeneralModel;
@@ -17,16 +23,30 @@ using TheAirline.GraphicsModel.UserControlModel.MessageBoxModel;
 using TheAirline.Model.GeneralModel.Helpers;
 using TheAirline.GraphicsModel.Converters;
 
-namespace TheAirline
+namespace TheAirline.GraphicsModel.PageModel.PageFinancesModel
 {
-	public partial class PageFinances
-	{
-		public PageFinances()
-		{
+    /// <summary>
+    /// Interaction logic for PageFinances.xaml
+    /// </summary>
+    public partial class PageFinances : Page
+    {
+        private Airline Airline;
 
-			//this.InitializeComponent();
+        public PageFinances(Airline airline)
+        {
+            InitializeComponent();
+            this.Language = XmlLanguage.GetLanguage(new CultureInfo(AppSettings.GetInstance().getLanguage().CultureInfo, true).IetfLanguageTag);
 
-			// Insert code required on object creation below this point.
-		}
-	}
+            this.Airline = airline;
+            Page page = null;
+          using (FileStream fs = new FileStream("TheAirline\\GraphicsModel\\PageModel\\PageFinancesModel\\PageFinances.xaml", FileMode.Open, FileAccess.Read))
+            {
+            page = (Page)XamlReader.Load(fs);
+            }
+
+          string airlineCash = GameObject.GetInstance().HumanAirline.Money.ToString();
+          TextBox cashValue = (TextBox)page.FindName("cashValue");
+          cashValue.DataContext = airlineCash;
+        }
+    }
 }
