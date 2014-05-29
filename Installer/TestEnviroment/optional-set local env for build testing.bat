34618cf63e2130f3d8b9e5b5bfd5be0386d91e97
ipavlovi <ipavlovi@hotmail.com>
2014-05-29 15:06:55 +0200

    versionName format was incorrect during server builds.

--- Installer/TestEnviroment/optional-set local env for build testing.bat
+++ Installer/TestEnviroment/optional-set local env for build testing.bat
@@ -1,17 +1,17 @@
-###############################################################################################################
-#                                                                                                             #
-# Set machine-wide enviroment variables used by WiX to test how build will work in APPVEYOR build enviroment. #
-# For more info check http://www.appveyor.com/docs/environment-variables                                      #
-#                                                                                                             #
-###############################################################################################################
-#                                                                                                             #
-# You will have to restart Visual Studio after script execution in order to test installler creation locally  #
-#                                                                                                             #
-###############################################################################################################
+:: ###############################################################################################################
+:: #                                                                                                             #
+:: # Set machine-wide enviroment variables used by WiX to test how build will work in APPVEYOR build enviroment. #
+:: # For more info check http://www.appveyor.com/docs/environment-variables                                      #
+:: #                                                                                                             #
+:: ###############################################################################################################
+:: #                                                                                                             #
+:: # You will have to restart Visual Studio after script execution in order to test installler creation locally  #
+:: #                                                                                                             #
+:: ###############################################################################################################
 
 SETX CI	"Local test system" /M
-SETX APPVEYOR_BUILD_NUMBER "1.3.5.7" /M
-SETX APPVEYOR_BUILD_VERSION "1.3.5-000-test" /M
+SETX APPVEYOR_BUILD_NUMBER "21" /M
+SETX APPVEYOR_BUILD_VERSION "1.3.21" /M
 SETX APPVEYOR_REPO_NAME "TheAirline/TestRepo" /M
 SETX APPVEYOR_REPO_BRANCH "TestBranch" /M
 SETX APPVEYOR_REPO_COMMIT "123d45a6" /M
